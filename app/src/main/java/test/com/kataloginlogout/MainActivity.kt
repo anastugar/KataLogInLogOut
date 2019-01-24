f40273a3327e90bccf88c7d43f5be8872d812445
Ana Stuyck <ana.glownet@gmail.com>
2019-01-24 16:47:49 +0100

    Add travis config

--- app/src/main/java/test/com/kataloginlogout/MainActivity.kt
+++ app/src/main/java/test/com/kataloginlogout/MainActivity.kt
@@ -1,12 +1,35 @@
 package test.com.kataloginlogout
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    var userNameText = ""
+    var userPassText = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        userNameText = username.text.toString()
+
+        userPassText = userpass.text.toString()
+
+        loginBtn.setOnClickListener(){
+            loginUser(userNameText, userPassText)
+        }
+
+
+    }
+
+    fun loginUser(name: String, pass: String){
+
+
+        //val intent: Intent = Intent(applicationContext, ::class.java)
+        startActivity(intent)
+
     }
 }
